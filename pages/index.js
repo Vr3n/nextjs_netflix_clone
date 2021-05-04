ba9846d9ebfc801db8c1e2798c8fcc11f2e1c5f0
vr3n <vickyspatel@gmail.com>
2021-05-05 00:27:45 +0530

    Created Netflix Landing Page

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,107 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div
+      className="flex flex-col items-center justify-center min-h-screen py-2 bg-black"
+      style={{
+        backgroundImage:
+          "linear-gradient(rgb(0 0 0 / 70%), rgb(0 0 0 / 65%)), url(/hero_bg.jpg)",
+      }}
+    >
       <Head>
-        <title>Netflix Clone</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>
+          Netflix India - Watch Tv Shows Online, Watch Movies Online
+        </title>
+        <link rel="icon" href="/nficon2016.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center bg-black">
-        <h1 className="text-4xl font-bold">
+      <nav className="flex max-w-[1300px] justify-between w-full p-5">
+        <span className="netflix_logo">
+          <svg
+            viewBox="0 0 150 30"
+            className="svg-icon svg-icon-netflix-logo"
+            focusable="false"
+            fill="#e50914"
+            width="10rem"
+            height="2rem"
+          >
+            <g id="netflix-logo">
+              <path
+                d="M105.06233,14.2806261 L110.999156,30 C109.249227,29.7497422 107.500234,29.4366857 105.718437,29.1554972 L102.374168,20.4686475 L98.9371075,28.4375293 C97.2499766,28.1563408 95.5928391,28.061674 93.9057081,27.8432843 L99.9372012,14.0931671 L94.4680851,-5.68434189e-14 L99.5313525,-5.68434189e-14 L102.593495,7.87421502 L105.874965,-5.68434189e-14 L110.999156,-5.68434189e-14 L105.06233,14.2806261 Z M90.4686475,-5.68434189e-14 L85.8749649,-5.68434189e-14 L85.8749649,27.2499766 C87.3746368,27.3437061 88.9371075,27.4055675 90.4686475,27.5930265 L90.4686475,-5.68434189e-14 Z M81.9055207,26.93692 C77.7186241,26.6557316 73.5307901,26.4064111 69.250164,26.3117443 L69.250164,-5.68434189e-14 L73.9366389,-5.68434189e-14 L73.9366389,21.8745899 C76.6248008,21.9373887 79.3120255,22.1557784 81.9055207,22.2804387 L81.9055207,26.93692 Z M64.2496954,10.6561065 L64.2496954,15.3435186 L57.8442216,15.3435186 L57.8442216,25.9996251 L53.2186709,25.9996251 L53.2186709,-5.68434189e-14 L66.3436123,-5.68434189e-14 L66.3436123,4.68741213 L57.8442216,4.68741213 L57.8442216,10.6561065 L64.2496954,10.6561065 Z M45.3435186,4.68741213 L45.3435186,26.2498828 C43.7810479,26.2498828 42.1876465,26.2498828 40.6561065,26.3117443 L40.6561065,4.68741213 L35.8121661,4.68741213 L35.8121661,-5.68434189e-14 L50.2183897,-5.68434189e-14 L50.2183897,4.68741213 L45.3435186,4.68741213 Z M30.749836,15.5928391 C28.687787,15.5928391 26.2498828,15.5928391 24.4999531,15.6875059 L24.4999531,22.6562939 C27.2499766,22.4678976 30,22.2495079 32.7809542,22.1557784 L32.7809542,26.6557316 L19.812541,27.6876933 L19.812541,-5.68434189e-14 L32.7809542,-5.68434189e-14 L32.7809542,4.68741213 L24.4999531,4.68741213 L24.4999531,10.9991564 C26.3126816,10.9991564 29.0936358,10.9054269 30.749836,10.9054269 L30.749836,15.5928391 Z M4.78114163,12.9684132 L4.78114163,29.3429562 C3.09401069,29.5313525 1.59340144,29.7497422 0,30 L0,-5.68434189e-14 L4.4690224,-5.68434189e-14 L10.562377,17.0315868 L10.562377,-5.68434189e-14 L15.2497891,-5.68434189e-14 L15.2497891,28.061674 C13.5935889,28.3437998 11.906458,28.4375293 10.1246602,28.6868498 L4.78114163,12.9684132 Z"
+                id="Fill-14"
+              ></path>
+            </g>
+          </svg>
+        </span>
+        <span className="cta flex">
+          <div className="px-2 mr-10 py-1 rounded-[5px] cursor-pointer border-[1px] border-white text-white flex align-center items-center text-md justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-3 mr-1"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+              />
+            </svg>
+            English
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-3 ml-1"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </div>
+          <div className="bg-[#e50914] text-md rounded-[3px] cursor-pointer px-4 py-2 text-white">
+            Sign In
+          </div>
+        </span>
+      </nav>
+
+      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center text-lg text-white">
+        <h1 className="text-6xl font-bold max-w-[40rem] font-lg">
           Unlimited movies, TV shows, and more.
         </h1>
-        <p className="text-2xl">Watch anywhere, Cancel anytime.</p>
+        <h2 className="text-2xl mt-4 mb-8">Watch anywhere. Cancel anytime.</h2>
+        <p className="text-white ">
+          Ready to watch? Enter your email to create or restart your membership
+        </p>
+        <div className="flex md:flex-row flex-col mt-4">
+          <input
+            placeholder="Email address"
+            className="bg-white p-4 mb-2 md:mb-0 md:min-w-[400px] min-w-[300px] text-black"
+          />
+          <button className="flex p-4 items-center bg-[#e50914] text-white text-lg align-center md:mt-0">
+            Get Started
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          </button>
+        </div>
       </main>
     </div>
   );
